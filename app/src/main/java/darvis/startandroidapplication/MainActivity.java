c16bb60d337c53002954117cad94b3c79b8cd3a0
darvis994 <darvis994@gmail.com>
2016-09-10 04:38:44 +0300

    Say Hello Button.

--- app/src/main/java/darvis/startandroidapplication/MainActivity.java
+++ app/src/main/java/darvis/startandroidapplication/MainActivity.java
@@ -2,14 +2,26 @@ package darvis.startandroidapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textViewHello;
+    Button buttonHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        textViewHello = (TextView) findViewById(R.id.helloTextView);
+        buttonHello = (Button) findViewById(R.id.helloButton);
 
-        System.out.println("Test commit 3");
+        buttonHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewHello.setText("Hello Genymotion!");
+            }
+        });
     }
 }
