20927f51a1429f1b5a1edcff5c85e1482d6ceebb
darvis994 <darvis994@gmail.com>
2016-09-10 05:20:23 +0300

    Calc - Sum

--- app/src/main/java/darvis/startandroidapplication/MainActivity.java
+++ app/src/main/java/darvis/startandroidapplication/MainActivity.java
@@ -4,11 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textViewHello;
     Button buttonHello;
+    EditText editText;
+
+    static int sum = 0;
+
+    void sum(String number) {
+        int input = Integer.parseInt(number);
+        sum = sum + input;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +25,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main);
         textViewHello = (TextView) findViewById(R.id.helloTextView);
         buttonHello = (Button) findViewById(R.id.helloButton);
-        
+        editText = (EditText) findViewById(R.id.inputTextField);
 
         buttonHello.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewHello.setText("Hello Genymotion!");
+                sum(editText.getText().toString().trim());
+                textViewHello.setText(String.valueOf(sum));
             }
         });
     }
